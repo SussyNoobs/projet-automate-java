d9ac691bec9a14096aa70f99bc5ae11df185e050
AssahiAbdelhak <bdalhqa89@gmail.com>
2024-01-01 18:40:22 +0100

    update and make automate1D more dynamic

--- src/Automate1D.java
+++ src/Automate1D.java
@@ -1,58 +1,93 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Automate1D {
-    public static int getNewState(String state){
 
-        switch(state){
-            case "110":
-            case "101":
-            case "011":
-            case "010":
-            case "001":
-                return 1;
-            case "111":
-            case "100":
-            case "000":
-                return 0;
+    public static int[] generateAnArrayFromString(String initialArray){
+        int array[] = new int[initialArray.length()];
+        int i = 0;
+        for(char c : initialArray.toCharArray()){
+            array[i] = Integer.parseInt(String.valueOf(c));
+            i++;
         }
+        return array;
+    }
+    public static String binaryFromInt(int rule){
+        String result = Integer.toBinaryString(rule);
+        if(result.length() < 8){
+            int length = result.length();
+            for(int i = 0 ; i < 8 - length ; i++){
+                System.out.println(result);
+                result = "0"+result;
+            }
+        }
+        return result;
+    }
 
-        return -1;
+    public static HashMap<String,Integer> initializeConfig(String binary){
+        HashMap<String,Integer> myMap = new HashMap<String,Integer>();
+
+
+        myMap.put("111",Integer.parseInt(String.valueOf(binary.toCharArray()[0])));
+        myMap.put("110",Integer.parseInt(String.valueOf(binary.toCharArray()[1])));
+        myMap.put("101",Integer.parseInt(String.valueOf(binary.toCharArray()[2])));
+        myMap.put("100",Integer.parseInt(String.valueOf(binary.toCharArray()[3])));
+        myMap.put("011",Integer.parseInt(String.valueOf(binary.toCharArray()[4])));
+        myMap.put("010",Integer.parseInt(String.valueOf(binary.toCharArray()[5])));
+        myMap.put("001",Integer.parseInt(String.valueOf(binary.toCharArray()[6])));
+        myMap.put("000",Integer.parseInt(String.valueOf(binary.toCharArray()[7])));
+
+        return myMap;
+    }
+
+    public static int getNewState(HashMap<String,Integer> myMap,String state){
+
+        return myMap.get(state);
+
     }
 
     public static void main(String[] args) {
-        int rule = 111;
-        int array[] = new int[10];
-        int newArray[] = new int[10];
-        array[5] = 1;
-        String ruleInBinary = Integer.toBinaryString(rule);
-        newArray = array.clone();
-        for(int j = 0; j < 10 ; j++){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Saisir le tableau initiale : ");
+        String initialArray = scanner.nextLine();
+
+        System.out.println("Saisir la règle : ");
+        int rule = scanner.nextInt();
+
+        System.out.println("Saisir le nombre de génération à simuler : ");
+        int generationNb = scanner.nextInt();
+
+        HashMap<String,Integer> myMap = initializeConfig(binaryFromInt(rule));
+
+        int array[] = generateAnArrayFromString(initialArray);
+        int newArray[] = array.clone();
+        int length = array.length;
+        for(int j = 0; j < length; j++){
             System.out.print(newArray[j]);
         }
         System.out.println("");
-        for(int i =0 ; i < 5 ; i++){
-            System.out.println("before : ");
-            for(int j = 0; j < 10 ; j++){
-                System.out.print(newArray[j]);
-            }
+        for(int i =0 ; i < generationNb ; i++){
+
             //System.out.println("");
             array = newArray.clone();
-            for(int j = 0; j < 10 ; j++){
+            for(int j = 0; j < length; j++){
                 int elem = array[j];
                 int before,after;
                 if(j==0){
-                    before = array[9];
+                    before = array[length - 1];
                     after = array[j+1];
-                }else if(j==9){
+                }else if(j==length - 1){
                     before = array[j-1];
                     after = array[0];
                 }else{
                     before = array[j-1];
                     after = array[j+1];
                 }
-                newArray[j] = getNewState(before+""+elem+""+after);
-                //System.out.println(before+""+elem+""+after+" devinet : "+newArray[j]);
+                newArray[j] = getNewState(myMap,before+""+elem+""+after);
             }
             //System.out.println("after : ");
-            for(int j = 0; j < 10 ; j++){
+            for(int j = 0; j < length; j++){
                 System.out.print(newArray[j]);
             }
             System.out.println("");
