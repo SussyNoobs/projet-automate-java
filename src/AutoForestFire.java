f67ea52a16c0405290d855d7ec79c2fc37edf464
AssahiAbdelhak <bdalhqa89@gmail.com>
2024-01-03 22:04:21 +0100

    update autoForestFire

--- src/AutoForestFire.java
+++ src/AutoForestFire.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class AutoForestFire {
-    public static boolean voisinEnFeu(int matrix[][],int i,int j){
+    enum STATE {
+        EMPTY,
+        FOREST,
+        FIRE,
+        BURNT
+    };
+
+    public static int neighboursOnFire(STATE matrix[][], int i,int j){
+        int nb = 0;
+        if(i - 1 >= 0 && matrix[i-1][j] == STATE.FIRE)
+            nb++;
+        if(j - 1 >= 0 && matrix[i][j-1] == STATE.FIRE)
+            nb++;
+        if(i + 1 < matrix.length && matrix[i+1][j] == STATE.FIRE)
+            nb++;
+        if(j + 1 < matrix.length && matrix[i][j+1] == STATE.FIRE)
+            nb++;
+        return nb;
+    }
+
+    public static boolean shouldBeOnFire(STATE matrix[][],int i,int j,int p,int q){
+
+        int neighboursOnFire = neighboursOnFire(matrix,i,j);
+        Random random = new Random();
+        return (q + p*neighboursOnFire) > random.nextInt(100);
+        /*
         if(i == 0 && j == 0){
             return matrix[i][j+1] == 2 || matrix[i+1][j] == 2;
         }else if(i == 0 && j == 9){
@@ -11,8 +40,6 @@ public class AutoForestFire {
         }else if(j == 9 && i == 9){
             return matrix[i][j-1] == 2 || matrix[i-1][j] == 2;
         }else if(i == 9){
-            //System.out.println(i + " "+ j);
-            //System.out.println(matrix[i][j-1] + " " +matrix[i][j+1] +" "+ matrix[i-1][j]);
             return matrix[i][j-1] == 2 ||matrix[i][j+1] == 2 || matrix[i-1][j] == 2;
         }else if(j == 9){
             return matrix[i][j-1] == 2 ||matrix[i-1][j] == 2 || matrix[i+1][j] == 2;
@@ -20,24 +47,24 @@ public class AutoForestFire {
             return matrix[i][j+1] == 2 ||matrix[i-1][j] == 2 || matrix[i+1][j] == 2;
         }else{
             return matrix[i][j+1] == 2 || matrix[i+1][j] == 2 || matrix[i][j-1] == 2 || matrix[i-1][j] == 2;
-        }
+        }*/
     }
 
-    public static void copyMatrix(int matrix[][],int newMatrix[][]){
-        for(int i =0 ; i < 10 ; i++)
+    public static void copyMatrix(STATE matrix[][],STATE newMatrix[][]){
+        for(int i =0 ; i < matrix.length ; i++)
             newMatrix[i] = matrix[i].clone();
     }
 
-    public static void printMatrix(int matrix[][],int taille){
-        for(int i =0 ; i < taille ; i++){
-            for(int j =0 ; j < taille ; j++){
-                if(matrix[i][j] == 0)
+    public static void printMatrix(STATE matrix[][]){
+        for(int i =0 ; i < matrix.length ; i++){
+            for(int j =0 ; j < matrix.length ; j++){
+                if(matrix[i][j] == STATE.EMPTY)
                     System.out.print("VI ");
-                else if(matrix[i][j] == 1)
+                else if(matrix[i][j] == STATE.FOREST)
                     System.out.print("FO ");
-                else if(matrix[i][j] == 2)
+                else if(matrix[i][j] == STATE.FIRE)
                     System.out.print("FE ");
-                else if(matrix[i][j] == 3)
+                else if(matrix[i][j] == STATE.BURNT)
                     System.out.print("BR ");
             }
             System.out.println(" ");
@@ -45,38 +72,79 @@ public class AutoForestFire {
         System.out.println("");
         System.out.println("");System.out.println("");
     }
-    public static void main(String[] args) {
-        int matrix[][] = new int[10][10];
-        int newMatrix[][] = new int[10][10];
-        for(int k =0 ; k < 10 ; k++){
-            for(int j =0 ; j < 10 ; j++){
-                matrix[k][j] = 0 + (int)(Math.random() * (4 - 0));
+
+    public static STATE[][] generateMatrix(int taille,int forestProportion,int fireProportion){
+        STATE[][] matrix = new STATE[taille][taille];
+        for(int i = 0; i < taille; i++)
+            Arrays.fill(matrix[i],STATE.EMPTY);
+        Random random = new Random();
+        int nbForest = ( forestProportion * (taille * taille ) ) / 100;
+        while(nbForest > 0){
+            if(matrix[random.nextInt(taille)][random.nextInt(taille)] == STATE.EMPTY){
+                matrix[random.nextInt(taille)][random.nextInt(taille)] = STATE.FOREST;
+                nbForest--;
             }
         }
+
+        int nbFire = ( fireProportion * (taille * taille ) ) / 100;
+        while(nbFire > 0){
+            if(matrix[random.nextInt(taille)][random.nextInt(taille)] == STATE.EMPTY){
+                matrix[random.nextInt(taille)][random.nextInt(taille)] = STATE.FIRE;
+                nbFire--;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir la proportion de la foret : (pourcentage entre 10 et 80)");
+        int forestProportion = scanner.nextInt();
+
+        System.out.println("Saisir la proportion du feu : (pourcentage entre 1 et 50)");
+        int fireProportion = scanner.nextInt();
+
+        System.out.println("Saisir la probabilité de propagation du feu p : ");
+        int p = scanner.nextInt();
+
+        System.out.println("Saisir la probabilité q (que une cellule foret passe en feu meme si aucune de ses voisines n'est en feu) : ");
+        int q = scanner.nextInt();
+
+        System.out.println("Saisir la taille de la matrice : ");
+        int taille = scanner.nextInt();
+
+        System.out.println("Saisir le nombre de génération à simuler : ");
+        int generationNb = scanner.nextInt();
+
+
+
+        STATE matrix[][] = generateMatrix(taille,forestProportion,fireProportion);
+        STATE newMatrix[][] = new STATE[taille][taille];
+
         copyMatrix(matrix,newMatrix);
 
-        printMatrix(matrix,10);
+        printMatrix(matrix);
 
-        for(int l = 0 ; l < 5 ; l++){
+        for(int l = 0 ; l < generationNb ; l++){
             copyMatrix(newMatrix,matrix);
-            for(int i =0 ; i < 10 ; i++){
-                for(int j =0 ; j < 10 ; j++){
-                    if(matrix[i][j] == 0){
-                        newMatrix[i][j] = 0;
-                    }else if(matrix[i][j] == 3){
-                        newMatrix[i][j] = 3;
-                    }else if(matrix[i][j] == 2){
-                        newMatrix[i][j] = 3;
-                    }else if(voisinEnFeu(matrix,i,j) && matrix[i][j] == 1){
-                        //System.out.println(i+"   "+j);
-                        newMatrix[i][j] = 2;
+            for(int i =0 ; i < taille ; i++){
+                for(int j =0 ; j < taille ; j++){
+                    if(matrix[i][j] == STATE.EMPTY){
+                        newMatrix[i][j] = STATE.EMPTY;
+                    }else if(matrix[i][j] == STATE.BURNT){
+                        newMatrix[i][j] = STATE.BURNT;
+                    }else if(matrix[i][j] == STATE.FIRE){
+                        newMatrix[i][j] = STATE.BURNT;
+                    }else if(shouldBeOnFire(matrix,i,j,p,q)){
+                        newMatrix[i][j] = STATE.FIRE;
                     }else{
-                        newMatrix[i][j] = 1;
+                        newMatrix[i][j] = STATE.FOREST;
                     }
 
                 }
             }
-            printMatrix(newMatrix,10);
+            printMatrix(newMatrix);
         }
     }
 }
